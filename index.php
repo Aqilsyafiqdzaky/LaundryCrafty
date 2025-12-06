3aeee8b877d13dcc26697c7d6950d4b9c19909c2
Aqilsyafiqdzaky <aqildzaky010406@gmail.com>
2025-12-07 00:59:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "db.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>LaundryCrafty</title>
+</head>
+<body>
+    <h1>Selamat Datang di LaundryCrafty (Admin)</h1>
+
+    <a href="pelanggan/">Data Pelanggan</a>
+    <a href="cucian/">Data Cucian</a>
+    <a href="transaksi/">Data Transaksi</a>
+</body>
+</html>
